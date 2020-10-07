d82a43c094d16c80d0bc06a8977b24ad632fc191
Pradeep Jerome <prjerome@gmail.com>
2020-10-07 23:10:23 +0200

    Passing all TDD tests in test.js for server/checkMillionDollarIdea.js. See comments in latter file.

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,12 +1,36 @@
-const checkMillionDollarIdea = (numWeeks, weeklyRevenue) => {
+//passing test.js line 517
+const checkMillionDollarIdea = (req, response, nextCallback) => {
+// passing test.js line 523
+    if(req.body.numWeeks*req.body.weeklyRevenue < 1000000) {
+        response.status(400).send();
+    }
+// passing test.js line 533    
+    else if(req.body.numWeeks*req.body.weeklyRevenue >= 1000000) {
+        nextCallback();   
+    }
+// passing test.js line 541
+    else if (!req.body.numWeeks || !req.body.weeklyRevenue) {
+        response.status(400).send();
+    }
+
+    else if (req.body.numWeeks && req.body.weeklyRevenue == 'invalid') {
+        response.status(400).send();                
+    }
+    
+    else if (req.body.numWeeks == 'invalid' && req.body.weeklyRevenue ) {
+        response.status(400).send();  
+    }              
+};
+
+/*const checkMillionDollarIdea = (numWeeks, weeklyRevenue) => {
     const totalValueIdea = numWeeks * weeklyRevenue;
     if(totalValueIdea > 1000000) {
-        
+        return true;
     }
     else {
-        console.log('Idea not worth a million');
+        return false;
     }
-};
+}; */
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
