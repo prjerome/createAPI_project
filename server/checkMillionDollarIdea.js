580259a829bed025218e7b26c4e0bf52ce9a6838
prjerome <66998016+prjerome@users.noreply.github.com>
2020-10-08 15:09:33 +0200

    Update checkMillionDollarIdea.js

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -22,15 +22,4 @@ const checkMillionDollarIdea = (req, response, nextCallback) => {
     }              
 };
 
-/*const checkMillionDollarIdea = (numWeeks, weeklyRevenue) => {
-    const totalValueIdea = numWeeks * weeklyRevenue;
-    if(totalValueIdea > 1000000) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}; */
-
-// Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
